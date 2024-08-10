d04d0de3fcd32a46328e736a9f457ee0d6537061
Priyank Thakkar <priyankthakkar@gmail.com>
2024-08-10 15:55:52 +1000

    adding routes and layout for sign-in and sign-up along with a specific layout for pages related to authentication.

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <h1 className="h1-bold">Sign In</h1>;
+};
+
+export default Page;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <h1 className="h1-bold">Sign Up</h1>;
+};
+
+export default Page;
