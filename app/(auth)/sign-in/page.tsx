c9f55b64bca2bc85e02c137b36c75a643981f077
Priyank Thakkar <priyankthakkar@gmail.com>
2024-08-11 12:46:08 +1000

    setting up auth with clerk

--- app/(auth)/sign-in/page.tsx
+++
@@ -1,5 +0,0 @@
-const Page = () => {
-  return <h1 className="h1-bold">Sign In</h1>;
-};
-
-export default Page;
--- app/(auth)/sign-up/page.tsx
+++
@@ -1,5 +0,0 @@
-const Page = () => {
-  return <h1 className="h1-bold">Sign Up</h1>;
-};
-
-export default Page;
