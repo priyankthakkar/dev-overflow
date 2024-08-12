b87a436c0f9c5d9b4fdae6098703d318da4cb0e4
Priyank Thakkar <priyankthakkar@gmail.com>
2024-08-13 00:40:12 +1000

    Setting up fonts, and website metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,29 @@ import {
 } from "@clerk/nextjs";
 import React from "react";
 import "./globals.css";
+import { Metadata } from "next";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "Dev Overflow",
+  description: "Ask and answer programming related questions",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -14,9 +37,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          formActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <header>
             <SignedOut>
               <SignInButton />
